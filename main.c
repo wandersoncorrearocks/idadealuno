299d474d6d257ff08c72acc9ac7c55c75a25c8b1
wandersoncorrearocks <wandersondossantos200@gmail.com>
2024-05-13 04:30:46 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main ()
+{   
+    int a=1;
+    
+    while(a!=0){
+        
+    printf("Type the students age\n");
+    scanf ("%d", &a);           //inputs the student's age
+    
+    if(a<5)
+        printf("The student has to be above 5 years old")
+    
+    else if(a>=5&&a<=7)
+      printf("Infant A\n");
+    
+    else if(a>=8&&a<=10)
+        printf("Infant B\n");
+    
+    else if (a<=1&&a<=13)
+        printf("Juvenile A\n");
+    
+    else if (a>=14&&a<=17)
+        printf("Juvenile B\n");
+    
+    else
+        printf("Adult\n");
+    }
+        
+  return 0;
+}
